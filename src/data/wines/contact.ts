7b5e76ff626b898b8055c327fb258f1979a6f72a
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-10-06 13:50:03 +0800

    add contact

--- src/data/wines/contact.ts
+++ src/data/wines/contact.ts
@@ -2,7 +2,7 @@ export const frontmatter = {
   sections: {
     contactUs: {
       heading: "Contáctanos!",
-      logo: "../img/logos/baple energy-logo.png",
+      logo: "../img/logos/baple-wines-logo.png",
       image: "../img/contact-us-1.jpg",
     },
   },
---
+++ src/pages/contact.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layouts/Layout";
+import ContactUs from "../components/sections/ContactUs";
+import ContactDetails from "../components/sections/ContactDetails";
+
+interface ContactProps {
+  data: {
+    javascriptFrontmatter: {
+      frontmatter: {
+        sections: {
+          contactUs: any,
+        },
+      },
+    },
+    site: {
+      siteMetadata: {
+        contact: any,
+      },
+    },
+  };
+}
+
+const ContactPage: React.SFC<ContactProps> = (({ data }) => {
+  const { contactUs } = data.javascriptFrontmatter.frontmatter.sections;
+  return (
+    <Layout>
+      <ContactUs
+        logo={contactUs.logo}
+        image={contactUs.image}
+        heading={contactUs.heading}
+      />
+      <ContactDetails contact={data.site.siteMetadata.contact}/>
+    </Layout>
+  );
+});
+
+export default ContactPage;
+
+export const query = graphql`
+  query ContactPageQuery {
+    site {
+      siteMetadata  {
+        contact {
+          phone
+          email
+          address
+          socialMedia {
+            facebook
+            twitter
+            instagram
+          }
+        }
+      }
+    }
+    javascriptFrontmatter(fileAbsolutePath: {regex: "/wines\/contact.ts/"}) {
+      frontmatter {
+        sections {
+          contactUs {
+            heading
+            image {
+              childImageSharp {
+                fluid(maxWidth: 1400) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            logo {
+              childImageSharp {
+                fixed(width: 300) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
